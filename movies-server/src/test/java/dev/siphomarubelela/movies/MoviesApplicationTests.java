a8b492b16e015640e7b136a2adcfdf3fbe4d70df
SiphoPrinceMarubelela <siphoprincemarubelela@gmail.com>
2023-12-11 10:27:12 +0200

    Uploaded FSA with complete server

---
+++ movies-server/src/test/java/dev/siphomarubelela/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.siphomarubelela.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
